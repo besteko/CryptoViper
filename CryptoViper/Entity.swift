4463a5e0b71c6056e05b15e29b842c1740ffe14a
Beste Kocaoğlu <bestekocaoglu@hotmail.com>
2023-07-29 11:20:25 +0300

    first commit

---
+++ CryptoViper/Entity.swift
@@ -0,0 +1,15 @@
+//
+//  Entity.swift
+//  CryptoViper
+//
+//  Created by Beste Kocaoglu on 21.07.2023.
+//
+
+import Foundation
+
+// Struct
+
+struct Crypto : Decodable {
+    let currency : String
+    let price : String
+}
